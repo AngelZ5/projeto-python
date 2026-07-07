bde1974b65be02ecbb4130c4362d48059c025eea
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-07-07 09:13:34 -0300

    alteração de idade

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ print("Sistema de verificação de idade")
 nome = input("Digite seu nome: ")
  
 idade = int(input("Digite sua idade: "))
-if idade >= 18:
+if idade >= 19:
     print(nome, "é maior de idade.")
 else:
     print(nome, "é menor de idade.")
