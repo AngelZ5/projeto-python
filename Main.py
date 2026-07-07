4d21cf457a377b64388f732eebb9fa745854a530
rodrigon-max <rodrigo.n@ba.estudante.senai.br>
2026-07-07 13:59:38 +0000

    adicionado comentário

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 print("Sistema de verificação de idade")
-nome = input("Digite seu nome: ")
+nome = input("Digite seu nome: ")#o usuário deve inserir seu nome
  
-idade = int(input("Digite sua idade: "))
-if idade >= 18:
+idade = int(input("Digite sua idade: "))#o usuário deve inserir sua idade
+if idade >= 18:#se o usuário tiver 18 anos ou mais, ele é maior de idade
     print(nome, "é maior de idade.")
-elif idade >=14:
+elif idade >=14:#se o usuário tiver 14 anos ou mais, ele é adolescente
     print(nome, "é adolescente.")
-else:
+else:#se o usuário tiver menos de 14 anos, ele é menor de idade
     print(nome, "é menor de idade.")
