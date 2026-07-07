c9d26c16f9c95d6aa4786b15269a819753bee7c9
Eros Tepes Santana <eros.tepes69@gmail.com>
2026-07-07 13:28:57 +0000

    Adicionando adolescente

--- Main.py
+++ Main.py
@@ -2,7 +2,9 @@ print("Sistema de verificação de idade")
 nome = input("Digite seu nome: ")
  
 idade = int(input("Digite sua idade: "))
-if idade >= 19:
+if idade >= 18:
     print(nome, "é maior de idade.")
+elif idade >=14:
+    print(nome, "é adolescente.")
 else:
     print(nome, "é menor de idade.")
